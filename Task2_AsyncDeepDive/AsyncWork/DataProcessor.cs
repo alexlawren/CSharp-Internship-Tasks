aed9ea1bc1ea85b3fba716384053f2363cf34ef9
Александр Лаврентьев <alexlawr284@gmail.com>
2025-10-04 21:58:04 +0300

    The second task is completed and update README with Task 2 (Async Demo)

---
+++ Task2_AsyncDeepDive/AsyncWork/DataProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncWork
+{
+    public class DataProcessor
+    {
+        private const int ProcessingTimeSeconds = 3;
+
+        public string ProcessData(string dataName)
+        {
+            Thread.Sleep(ProcessingTimeSeconds * 1000);
+            return CreateResult(dataName);
+        }
+
+        public async Task<string> ProcessDataAsync(string dataName)
+        {
+            await Task.Delay(ProcessingTimeSeconds * 1000);
+            return CreateResult(dataName);
+        }
+
+        public string CreateResult(string dataName)
+        {
+            return $"Обработка {dataName} завершена за {ProcessingTimeSeconds} секунды.";
+        }
+    }
+
+}
---
+++ Task2_AsyncDeepDive/AsyncWork/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncWork
+{
+    internal class Program
+    {
+        private static readonly List<string> DataToProcess = new List<string> { "Файл 1", "Файл 2", "Файл 3" };
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Демонстрация синхронного и асинхронного выполнения\n");
+            RunSynchronousDemo();
+            await RunAsynchronousDemo();
+            Console.WriteLine("\nНажмите любую клавишу для выхода.");
+            Console.ReadKey();
+        }
+
+        private static void RunSynchronousDemo()
+        {
+            Console.WriteLine("Начинаем синхронную обработку");
+            var processor = new DataProcessor();
+            var stopwatch = Stopwatch.StartNew();
+
+            foreach (var data in DataToProcess)
+            {
+                Console.WriteLine(processor.ProcessData(data));
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Синхронная обработка заняла: {stopwatch.Elapsed.TotalSeconds} секунд");
+        }
+
+        private static async Task RunAsynchronousDemo()
+        {
+            Console.WriteLine("Начинаем асинхронную обработку");
+            var processor = new DataProcessor();
+            var stopwatch = Stopwatch.StartNew();
+
+            var runningTask = DataToProcess.Select(data => processor.ProcessDataAsync(data)).ToList();
+
+            while (runningTask.Any())
+            {
+                Task<string> completedTask = await Task.WhenAny(runningTask);
+                Console.WriteLine(await completedTask);
+
+                runningTask.Remove(completedTask);
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"\nАсинхронная обработка заняла: {stopwatch.Elapsed.TotalSeconds} секунд.");
+        }
+
+    }
+}
